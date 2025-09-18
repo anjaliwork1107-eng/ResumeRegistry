2815e2d835c0c05ec997d20444f60d445a5aa2fa
anjaliwork1107-eng <anjaliwork.1107@gmail.com>
2025-09-18 16:52:21 +0530

    Update index.php

--- Resume_registry/index.php
+++ Resume_registry/index.php
@@ -144,7 +144,7 @@ $profiles = $stmt->fetchAll();
                 <a href="add.php">➕ Add New Entry</a>
                 <a href="logout.php">🚪 Logout</a>
             <?php else: ?>
-                <a href="login.php">Please log in</a>
+                <a href="login.php"><span aria-hidden="true">🔐</span> Please log in</a>
             <?php endif; ?>
         </div>
 
@@ -176,5 +176,6 @@ $profiles = $stmt->fetchAll();
 </body>
 
 </html>
+
 
 
